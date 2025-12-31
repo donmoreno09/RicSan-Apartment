3148aa7040d26a9cd89f57a08d4d77f757b43c59
Don Moreno <d.moreno@esis-italia.com>
2025-12-31 22:05:05 +0100

    refactor: fixed the routing so every route is rerouted to react router

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/**
+ * Catch-all route for React Router
+ * 
+ * Send all non-API routes to React app
+ * React Router will handle the routing client-side
+ */
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
